b4e1445610943e2792d7d2292ceffda7f3a41d66
mung96 <mung5079@gmail.com>
2025-01-19 19:51:01 +0900

    feat: 커리큘럼 item 구현

--- src/app/(with-header)/page.tsx
+++ src/app/(with-header)/page.tsx
@@ -3,6 +3,7 @@
 import Button from '@/components/Button';
 import PencilIcon from '@/app/assets/svg/PencilIcon.svg';
 import ListItem from '@/components/ListItem';
+import CurriculumItem, { PlayStatus } from '@/course/CurriculumItem';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Button>
       <ListItem label="리스트 아이템" icon={<PencilIcon />} isSelect={false} />
       <ListItem label="리스트 아이템" icon={<PencilIcon />} isSelect={true} />
+      <CurriculumItem title="커리큘럼 아이템" runTime={0} status={PlayStatus.COMPLETED} />
     </>
   );
 }
---
+++ src/course/CurriculumItem.tsx
@@ -0,0 +1,52 @@
+import { exhaustiveCheck } from '@/utils/typeGuard';
+import { cva } from 'class-variance-authority';
+import { twMerge } from 'tailwind-merge';
+
+export enum PlayStatus {
+  COMPLETED = 'COMPLETED',
+  IN_PROGRESS = 'IN_PROGRESS',
+  NOT_STARTED = 'NOT_STARTED',
+}
+
+const statusStyles = cva(``, {
+  variants: {
+    status: {
+      [PlayStatus.COMPLETED]: 'text-primary-500',
+      [PlayStatus.IN_PROGRESS]: 'text-neutral-gray-950',
+      [PlayStatus.NOT_STARTED]: 'text-secondary-500',
+    },
+  },
+});
+
+const getStatusLabel = (status: PlayStatus) => {
+  if (status == PlayStatus.NOT_STARTED) {
+    return '미수강';
+  }
+  if (status == PlayStatus.IN_PROGRESS) {
+    return '수강중';
+  }
+  if (status == PlayStatus.COMPLETED) {
+    return '수강완료';
+  } else {
+    exhaustiveCheck(status);
+    return '';
+  }
+};
+
+type Props = {
+  title: string;
+  runTime: number;
+  status: PlayStatus;
+};
+
+export default function CurriculumItem({ title, runTime, status }: Props) {
+  return (
+    <li className="flex items-center justify-between h-[58px] px-4 rounded-lg">
+      <p className="text-neutral-gray-950 subtitle2 tablet:subtitle3">{title}</p>
+      <div className="flex items-center gap-4">
+        <p className={twMerge('body1', statusStyles({ status }))}>{getStatusLabel(status)}</p>
+        <p className="body2">{runTime}분</p>
+      </div>
+    </li>
+  );
+}
