9d8a409cdf546ecaa7f31753095b6f04a5d1a7a2
mung96 <mung5079@gmail.com>
2025-01-22 00:12:18 +0900

    design: tablet, desktop 강좌상세 반응형 디자인

--- src/app/(with-header)/courses/[id]/page.tsx
+++ src/app/(with-header)/courses/[id]/page.tsx
@@ -8,36 +8,38 @@ const course = {
 };
 export default function Page() {
   return (
-    <main className="px-4 pb-[72px] pt-9 tablet:px-6 tablet:pt-12 desktop:pt-16">
-      <div className="flex flex-col gap-6">
-        <div className=" aspect-video w-full min-w-[163px] rounded-lg ">
-          <img className="size-full rounded-lg" src={course.thumbnail} alt={'강좌 썸네일'} />
+    <main className="">
+      <div className="tablet:flex tablet:flex-col tablet:items-center">
+        <div className="flex w-fit flex-col gap-6  px-4 pb-[72px] pt-9 tablet:w-[768px] tablet:flex-row tablet:justify-start tablet:gap-4 tablet:px-6 tablet:pt-12 desktop:w-[1280px] desktop:px-[128px] desktop:pt-16">
+          <div className=" aspect-video w-full min-w-[163px] rounded-lg tablet:h-[247px] tablet:w-[352px] desktop:w-[400px]">
+            <img className="size-full rounded-lg" src={course.thumbnail} alt={'강좌 썸네일'} />
+          </div>
+
+          <section className="flex flex-col gap-4 tablet:justify-end">
+            <h1 className="title1 text-neutral-gray-950">제목</h1>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-5">
+                <p className="body1 text-neutral-gray-800">수업시간</p>
+                <p className="body1 text-neutral-gray-800">
+                  {0}시간 {0}분
+                </p>
+              </div>
+              <div className="flex gap-5">
+                <p className="body1 text-neutral-gray-800">수강 기한</p>
+                <p className="body1 text-neutral-gray-800">무제한</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 tablet:flex-row tablet:gap-4">
+              <Button variant="filled" onClick={() => console.log('수강 신청')}>
+                수강 신청
+              </Button>
+              <Button variant="outlined" onClick={() => console.log('수강 신청')}>
+                강의 자료 다운로드
+              </Button>
+            </div>
+          </section>
         </div>
-
-        <section className="flex flex-col gap-4">
-          <h1 className="title1 text-neutral-gray-950">제목</h1>
-          <div className="flex flex-col gap-2">
-            <div className="flex gap-5">
-              <p className="body1 text-neutral-gray-800">수업시간</p>
-              <p className="body1 text-neutral-gray-800">
-                {0}시간 {0}분
-              </p>
-            </div>
-            <div className="flex gap-5">
-              <p className="body1 text-neutral-gray-800">수강 기한</p>
-              <p className="body1 text-neutral-gray-800">무제한</p>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <Button variant="filled" onClick={() => console.log('수강 신청')}>
-              수강 신청
-            </Button>
-            <Button variant="outlined" onClick={() => console.log('수강 신청')}>
-              강의 자료 다운로드
-            </Button>
-          </div>
-        </section>
       </div>
     </main>
   );
