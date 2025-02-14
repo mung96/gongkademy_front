057f677508077eea176d3e79120e92e7691e4d6a
mung96 <mung5079@gmail.com>
2025-02-14 14:45:01 +0900

    fix: 필요없는 api 삭제

--- src/app/(with-header)/page.tsx
+++ src/app/(with-header)/page.tsx
@@ -4,31 +4,7 @@ import Link from 'next/link';
 import { PATH } from '@/constants/path';
 import BoardItem from '@/board/BoardItem';
 import { BoardCategory, BoardCriteria } from '@/board/type';
-import { GetBoardListResponse } from '@/board/api';
-import { apiServerRequester } from '@/api/serverRequest';
-// import { getBoardListResponse } from '@/board/api';
-export async function getBoardListResponse(
-  boardCategory: BoardCategory,
-  page: number = 1,
-  criteria: BoardCriteria = BoardCriteria.CREATE_AT,
-  onSuccess?: (response: GetBoardListResponse) => void,
-  courseId?: number,
-  lectureId?: number,
-) {
-  try {
-    const response = await apiServerRequester.get<GetBoardListResponse>(
-      `/boards?category=${boardCategory}&page=${page}&criteria=${criteria}${courseId !== undefined ? '&course=' + courseId : ''}${lectureId !== undefined ? '&lecture=' + lectureId : ''}`,
-    );
-    if (onSuccess) {
-      onSuccess(response.data);
-    }
-    return response.data;
-  } catch (error) {
-    console.log('error발생');
-    console.log(error);
-  }
-  return { boardList: [], totalPage: 0 };
-}
+import { getBoardListResponse } from '@/board/api';
 
 async function getHomeBoardList() {
   const data = await getBoardListResponse(BoardCategory.WORRY, 1, BoardCriteria.CREATE_AT);
