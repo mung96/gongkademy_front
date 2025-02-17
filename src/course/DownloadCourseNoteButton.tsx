34a10ba10720fea5520289958e1ee59752d5a683
mung96 <mung5079@gmail.com>
2025-02-17 11:32:27 +0900

    fix: 필요없는 주석 삭제

--- src/course/DownloadCourseNoteButton.tsx
+++ src/course/DownloadCourseNoteButton.tsx
@@ -40,17 +40,6 @@ export default function DownloadCourseNoteButton({ courseId }: Props) {
     }
   };
 
-  // const handleDownload = () => {
-  //   const fileUrl =
-  //     'https://blog.kakaocdn.net/dn/bnqe5b/btr2RieadXb/l5TCrkNPyhwSb9VJWNcW00/%E1%84%8C%E1%85%A2%E1%84%85%E1%85%AD%E1%84%8B%E1%85%A7%E1%86%A8%E1%84%92%E1%85%A1%E1%86%A8%20%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%E1%84%8C%E1%85%A1%E1%84%85%E1%85%AD.pdf?attach=1&knm=tfile.pdf';
-  //   const link = document.createElement('a');
-  //   link.href = fileUrl;
-  //   link.setAttribute('download', '강의자료.pdf'); // 원하는 파일명으로 변경 가능
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   link.remove();
-  // };
-
   return (
     <Button variant="outlined" onClick={handleDownload}>
       <a ref={buttonRef}>강의 자료 다운로드</a>
