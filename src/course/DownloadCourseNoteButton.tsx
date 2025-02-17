c132462a1e82f020687db3f211251d8ca5ae3a88
mung96 <mung5079@gmail.com>
2025-02-17 11:29:43 +0900

    feat: 강좌 자료 다운 기능 추가

--- src/course/DownloadCourseNoteButton.tsx
+++ src/course/DownloadCourseNoteButton.tsx
@@ -1,32 +1,59 @@
 'use client';
 
-import Button from '@/components/Button';
 import { apiRequester } from '@/api/requester';
+import Button from '@/components/Button';
+import { HTTP_STATUS } from '@/constants/api';
+import { isAxiosError } from 'axios';
+import { useRef } from 'react';
+
+type DownloadCourseNoteResponse = {
+  courseNoteUrl: string;
+  courseNoteName: string;
+};
 
 type Props = {
   courseId: number;
 };
 
 export default function DownloadCourseNoteButton({ courseId }: Props) {
-  const handleDownload = async () => {
-    const response = await apiRequester.get(`/courses/${courseId}/note`, {
-      responseType: 'blob',
-    });
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement('a');
-    link.href = url;
-    const contentDisposition = response.headers['content-disposition'];
-    const fileName = contentDisposition.split('filename=').pop();
+  const buttonRef = useRef<HTMLAnchorElement>(null);
 
-    link.setAttribute('download', fileName); // 파일 이름 설정
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
+  const handleDownload = async () => {
+    if (buttonRef.current !== null && buttonRef.current.href === '') {
+      try {
+        const response = await apiRequester.get<DownloadCourseNoteResponse>(`/courses/${courseId}/note`);
+
+        if (buttonRef.current !== null) {
+          console.log(response.data);
+          buttonRef.current.href = response.data.courseNoteUrl;
+          buttonRef.current.click();
+        }
+      } catch (error) {
+        if (isAxiosError(error)) {
+          if (error.status === HTTP_STATUS.NOT_FOUND) {
+            if (error.response?.data.message === '수강 중인 강좌가 아닙니다.') {
+              alert('수강 신청 후 다운받을 수 있습니다.');
+            }
+          }
+        }
+      }
+    }
   };
+
+  // const handleDownload = () => {
+  //   const fileUrl =
+  //     'https://blog.kakaocdn.net/dn/bnqe5b/btr2RieadXb/l5TCrkNPyhwSb9VJWNcW00/%E1%84%8C%E1%85%A2%E1%84%85%E1%85%AD%E1%84%8B%E1%85%A7%E1%86%A8%E1%84%92%E1%85%A1%E1%86%A8%20%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%E1%84%8C%E1%85%A1%E1%84%85%E1%85%AD.pdf?attach=1&knm=tfile.pdf';
+  //   const link = document.createElement('a');
+  //   link.href = fileUrl;
+  //   link.setAttribute('download', '강의자료.pdf'); // 원하는 파일명으로 변경 가능
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   link.remove();
+  // };
 
   return (
     <Button variant="outlined" onClick={handleDownload}>
-      강의 자료 다운로드
+      <a ref={buttonRef}>강의 자료 다운로드</a>
     </Button>
   );
 }
