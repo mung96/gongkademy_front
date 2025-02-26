039208a6d0f6eef20859fbf37ad416f294ba8b73
mung96 <mung5079@gmail.com>
2025-02-26 20:02:31 +0900

    feat: footer 문의사항 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="body2 flex h-12 min-h-12 w-full items-center justify-center bg-neutral-gray-950 text-neutral-gray-0">
-      2025, GONGKADEMY
+    <footer className="body2 flex h-12 min-h-12 w-full items-center justify-center gap-4 bg-neutral-gray-950 text-neutral-gray-0">
+      <span>2025, GONGKADEMY</span>
+      <span>
+        문의하기 : <a href="mailto:gongkademy@gmail.com">gongkademy@gmail.com</a>
+      </span>
     </footer>
   );
 }
