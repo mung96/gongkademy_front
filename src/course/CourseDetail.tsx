e0a2237dd26493445d5ea9eb6811849dceff1775
mung96 <mung5079@gmail.com>
2025-03-25 17:35:10 +0900

    refactor: 강좌상세 API 요청 서버컴포넌트로 변경

--- src/course/CourseDetail.tsx
+++ src/course/CourseDetail.tsx
@@ -1,56 +1,22 @@
-'use client';
-
-import { apiRequester } from '@/api/requester';
+import { GetCourseDetailResponse } from '@/app/(with-header)/courses/[courseId]/[tab]/page';
 import LoadingComponent from '@/components/LoadingComponent';
 import DownloadCourseNoteButton from '@/course/DownloadCourseNoteButton';
 import RegisterCourseButton from '@/course/RegisterCourseButton';
 import { getCourseThumbnailPath } from '@/utils';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 type Props = {
   courseId: number;
-};
-type GetCourseDetailResponse = {
-  title: string;
-  thumbnail: string;
-  courseNote: string;
-  courseTime: number;
-  isRegister: boolean;
+  courseDetail: GetCourseDetailResponse;
 };
 
-async function getCourseDetailResponse(courseId: number, onSuccess: (data: GetCourseDetailResponse) => void) {
-  const response = await apiRequester.get<GetCourseDetailResponse>(`/courses/${courseId}`);
-  onSuccess(response.data);
-  return response.data;
-}
-
-export default function CourseDetail({ courseId }: Props) {
-  const [courseDetail, setCourseDetail] = useState<GetCourseDetailResponse | null>(null);
-
-  useEffect(() => {
-    getCourseDetailResponse(courseId, (courseDetail) => setCourseDetail(courseDetail));
-  }, []);
-
+export default function CourseDetail({ courseId, courseDetail }: Props) {
   if (courseDetail === null) {
     return <LoadingComponent />;
   }
 
   return (
     <div className="flex flex-col gap-6 tablet:flex-row tablet:justify-start tablet:gap-4 ">
-      {/* <div className="flex  w-full min-w-[163px] flex-col justify-end rounded-lg  tablet:max-h-[247px] tablet:w-[352px] desktop:w-[400px]">
-        {/* <Image
-          src={getCourseThumbnailPath(courseDetail.thumbnail)}
-          alt={`${courseDetail.title} 썸네일`}
-          width={400}
-          height={247}
-          layout="responsive"
-          objectFit="cover"
-          className="rounded-lg"
-          sizes="(min-width: 768px) 352px, (min-width: 1024px) 330px, 325px"
-          priority
-        />
-      </div>  */}
       <div className="relative aspect-[400/247] h-[247px] w-[400px] overflow-hidden rounded-lg">
         <Image
           src={getCourseThumbnailPath(courseDetail.thumbnail)}
